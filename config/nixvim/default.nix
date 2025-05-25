a55076c5c474bb0f6001f7f708752dc42fc9a82d
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-05-25 14:13:53 +0000

    feat(nixvim): add custom Neovim editor options
    Borrowed most of these from LazyVim
    
    - Save buffer automatically when switching
    - Completion menu behavior
    - Highlight current line
    - Use spaces instead of tabs
    - Characters for UI elements (diff, folds, etc)
    - Start with all folds open
    - Text formatting rules
    - Use ripgrep for :grep command
    - Case insensitive search
    - Restore view when jumping
    - Global statusline
    - Break lines at word boundaries
    - Show whitespace characters
    - Enable mouse in all modes
    - Show line numbers
    - Popup menu transparency
    - Show relative line numbers
    - Hide cursor position in statusline
    - Keep 4 lines visible when scrolling
    - What to save in sessions
    - Round indent to shiftwidth
    - Indent size
    - Abbreviate messages
    - Hide mode display
    - Horizontal scroll margin
    - Always show sign column
    - Case sensitive if uppercase used
    - Tab width in insert mode
    - Spell check language
    - New horizontal splits below
    - Keep splits on screen
    - New vertical splits to right
    - Tab display width
    - Enable 24-bit colors
    - Persistent undo history
    - Maximum undo levels
    - Faster CursorHold events
    - Allow cursor past end in visual block
    - Command completion behavior
    - Minimum window width
    - Disable line wrapping

--- config/nixvim/default.nix
+++ config/nixvim/default.nix
@@ -17,6 +17,56 @@
     globals.maplocalleader = ",";
     keymaps = [ ];
 
+    opts = {
+      autowrite = true;
+      completeopt = "menu,menuone,noselect";
+      cursorline = true;
+      expandtab = true;
+      fillchars = {
+        foldopen = "";
+        foldclose = "";
+        fold = " ";
+        foldsep = " ";
+        diff = "╱";
+        eob = " ";
+      };
+      foldlevel = 99;
+      formatoptions = "jcrqlnt";
+      grepprg = "rg --vimgrep";
+      ignorecase = true;
+      jumpoptions = "view";
+      laststatus = 3;
+      linebreak = true;
+      list = true;
+      mouse = "a";
+      number = true;
+      pumblend = 20;
+      relativenumber = true;
+      ruler = false;
+      scrolloff = 4;
+      sessionoptions = [ "buffers" "curdir" "tabpages" "winsize" "help" "globals" "skiprtp" "folds" ];
+      shiftround = true;
+      shiftwidth = 2;
+      shortmess = "ltToOcCFWI";
+      showmode = false;
+      sidescrolloff = 4;
+      signcolumn = "yes";
+      smartcase = true;
+      softtabstop = 2;
+      spelllang = "en";
+      splitbelow = true;
+      splitkeep = "screen";
+      splitright = true;
+      tabstop = 2;
+      undofile = true;
+      undolevels = 10000;
+      updatetime = 200;
+      virtualedit = "block";
+      wildmode = "longest:full,full";
+      winminwidth = 5;
+      wrap = false;
+    };
+
     plugins = {
       lualine.enable = true;
       which-key.enable = true;
