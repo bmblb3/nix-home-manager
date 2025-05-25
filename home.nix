a8d619e0b03c3c6935a19f95c02678f0213c1c7b
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-05-25 11:40:16 +0000

    feat(nvim): add lazygit integration and keybinding
    - Added lazygit to extraPackages for availability in the environment.
    - Enabled the lazygit plugin in Neovim configuration.
    - Added a keymap (<leader>gg) to launch lazygit from within Neovim.
    - Provided a description and set the keymap to silent for better UX.

--- home.nix
+++ home.nix
@@ -129,6 +129,10 @@
     defaultEditor = true;
     viAlias = true;
 
+    extraPackages = with pkgs; [
+      lazygit
+    ];
+
     colorschemes.catppuccin.enable = true;
 
     plugins = {
@@ -137,11 +141,26 @@
       copilot-lua.enable = true;
       copilot-chat.enable = true;
 
+      lazygit.enable = true;
+
       which-key.enable = true;
 
     };
+
     globals.mapleader = " ";
     globals.maplocalleader = " ";
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>gg";
+        action = "<cmd>LazyGit<CR>";
+        options = {
+          silent = true;
+          desc = "Launch lazygit";
+        };
+      }
+    ];
+
   };
 
   programs.bash = {
