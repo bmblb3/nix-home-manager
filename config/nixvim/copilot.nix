404f150bd490e03679bb0ae133f169c0d3a8d5fc
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-05-25 12:45:34 +0000

    feat(nixvim): add keymap for CopilotChatCommit
    Introduce a new keymap <leader>aic to trigger the CopilotChatCommit
    command.

--- config/nixvim/copilot.nix
+++ config/nixvim/copilot.nix
@@ -6,7 +6,16 @@
     plugins.copilot-lua.enable = true;
     plugins.copilot-chat.enable = true;
 
-    keymaps = [ ];
+    keymaps = [
+      {
+        key = "<leader>aic";
+        action = "<cmd>:CopilotChatCommit<CR>";
+        options = {
+          silent = true;
+          desc = "[AI]: Generate [c]ommit message";
+        };
+      }
+    ];
 
   };
 }
