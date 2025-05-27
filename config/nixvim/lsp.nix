5b47e6817831d747040b606fe63df3a0b086e603
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-05-27 06:34:06 +0000

    feat(nixvim): add conform-nvim for formatting support
    - Used in LazyVim
    - Update format keymap to use conform-nvim
    - Enable notifications for formatting errors
    and missing formatters.

--- config/nixvim/lsp.nix
+++ config/nixvim/lsp.nix
@@ -4,12 +4,38 @@
 
   programs.nixvim = {
 
+    extraPackages = with pkgs; [
+      ruff
+      nixfmt-rfc-style
+    ];
+
     plugins.lspconfig.enable = true;
+
+    plugins.conform-nvim = {
+      enable = true;
+
+      settings = {
+        format_on_save = {
+          lsp_format = "fallback";
+          timeout_ms = 500;
+        };
+        default_format_opts = {
+          lsp_format = "fallback";
+        };
+        notify_on_error = true;
+        notify_no_formatters = true;
+
+        formatters_by_ft = {
+          python = [ "ruff_format" ];
+          nix = [ "nixfmt" ];
+        };
+      };
+    };
 
     keymaps = [
       {
         key = "<leader>lf";
-        action.__raw = "vim.lsp.buf.format";
+        action = "<cmd>require('conform').format(lsp_format = 'fallback')<CR>";
         options = {
           silent = true;
           desc = "[L]sp: [F]ormat";
