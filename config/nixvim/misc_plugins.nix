6fe86a0b85637621410f86be650eea1adeeab942
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-05-27 16:44:46 +0000

    feat(nixvim): add jump, remote keymaps to flash similar LazyVim

--- config/nixvim/misc_plugins.nix
+++ config/nixvim/misc_plugins.nix
@@ -15,7 +15,72 @@
         };
       };
 
-      flash.enable = true;
+      flash = {
+        enable = true;
+        settings = {
+          labels = "asdfghjklqwertyuiopzxcvbnm";
+        };
+      };
     };
+
+    keymaps = [
+      {
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
+        key = "s";
+        action = "<cmd>lua require('flash').jump()<CR>";
+        options = {
+          silent = true;
+          desc = "Flash: Jump";
+        };
+      }
+      {
+        mode = [
+          "n"
+          "x"
+          "o"
+        ];
+        key = "S";
+        action = "<cmd>lua require('flash').treesitter()<CR>";
+        options = {
+          silent = true;
+          desc = "Flash: Treesitter";
+        };
+      }
+
+      {
+        mode = [ "o" ];
+        key = "r";
+        action = "<cmd>lua require('flash').remote()<CR>";
+        options = {
+          silent = true;
+          desc = "Flash: Remote";
+        };
+      }
+      {
+        mode = [
+          "x"
+          "o"
+        ];
+        key = "R";
+        action = "<cmd>lua require('flash').treesitter_search()<CR>";
+        options = {
+          silent = true;
+          desc = "Flash: Treesitter search";
+        };
+      }
+      {
+        mode = [ "c" ];
+        key = "<c-s>";
+        action = "<cmd>lua require('flash').togglt()<CR>";
+        options = {
+          silent = true;
+          desc = "Flash: Treesitter toggle";
+        };
+      }
+    ];
   };
 }
