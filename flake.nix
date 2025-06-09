6eed92fdbec61134e77c284067a75da488082b25
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-06-09 17:44:26 +0000

    feat(flake): add extra cachix substituters and keys
    Added extra-substituters and extra-trusted-public-keys to nixConfig in
    flake.nix for improved binary cache support. This enables faster builds
    by using devenv.cachix.org and nixpkgs-python.cachix.org caches.

--- flake.nix
+++ flake.nix
@@ -2,6 +2,11 @@
 
   description = "NixOS configuration";
 
+  nixConfig.extraOptions = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw= nixpkgs-python.cachix.org-1:hxjI7pFxTyuTHn2NkvWCrAUcNZLNS3ZAvfYNuYifcEU=
+  '';
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixos-wsl.url = "github:nix-community/NixOS-WSL/main";
