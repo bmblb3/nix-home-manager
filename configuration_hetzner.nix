2540d959f48b18fc21d1df7fc9f4ec2a011ec6e1
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-07-01 20:14:40 +0000

    feat(hetzner): add NixOS config for Hetzner deployment
    Introduce configuration files for deploying NixOS on Hetzner, including
    hardware configuration, disk layout using disko, and a dedicated system
    configuration. Update flake.nix to add a Hetzner system target, integrate
    disko, and set up user and home-manager options for the new host.

---
+++ configuration_hetzner.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+  imports = [
+    ./hardware-configuration_hetzner.nix
+    ./disko-config_hetzner.nix
+    ./configuration_base.nix
+  ];
+
+  boot.loader.grub.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings.X11Forwarding = true;
+  };
+
+  services.cron.enable = true;
+}
--- flake.nix
+++ flake.nix
@@ -11,6 +11,9 @@
 
     nixvim.url = "github:nix-community/nixvim/nixos-25.05";
     nixvim.inputs.nixpkgs.follows = "nixpkgs";
+
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
@@ -55,6 +58,39 @@
             inherit nixos-wsl;
             hostname = "wsl-work";
             username = "akucwh";
+          };
+        };
+
+        hetzner = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+
+            ./configuration_hetzner.nix
+            inputs.disko.nixosModules.disko
+
+            {
+              nix.settings.trusted-users = [
+                "root"
+                "bmblb3"
+              ];
+              users.users.bmblb3.extraGroups = [ "docker" ];
+            }
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.bmblb3 = ./home.nix;
+              home-manager.extraSpecialArgs = {
+                username = "bmblb3";
+                inherit nixvim;
+              };
+            }
+
+          ];
+          specialArgs = {
+            hostname = "hetzner";
+            username = "bmblb3";
           };
         };
 
