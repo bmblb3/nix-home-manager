02a9e77a4da55bd6dbb34bcca1472f1213344c9c
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-08-15 08:46:28 +0000

    fix: forgot to add flake.nix to the prev commit

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixCats-nvim.url = "git+ssh://git@github.com/bmblb3/nvim_nixcats.git?ref=main";
+    nixCats-nvim.url = "git+https://github.com/bmblb3/nvim_nixcats?ref=main";
     nixCats-nvim.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
