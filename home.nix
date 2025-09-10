9ba69c6c4b1570f644a46039e0e4945d43b5d912
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-10 11:07:55 +0000

    kitty use jetbrains mono

--- home.nix
+++ home.nix
@@ -44,6 +44,7 @@
     libtool
     lsof
     moreutils
+    nerd-fonts.jetbrains-mono
     ncdu
     nix-output-monitor
     nodejs_22
@@ -224,6 +225,8 @@
     };
   };
 
+  fonts.fontconfig.enable = true;
+
   programs.kitty = {
     enable = true;
     font = {
@@ -239,6 +242,7 @@
       map shift+left previous_tab
       map shift+right next_tab
       map shift+f2 set_window_title
+      symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 JetBrainsMono Nerd Font Mono
     '';
   };
 
