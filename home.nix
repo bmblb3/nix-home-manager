d5008b00ba2845009e834287fed2a22ac32edc17
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-09-16 09:06:46 +0000

    kitty as mkOutOfStoreSymlink

--- home.nix
+++ home.nix
@@ -45,6 +45,7 @@
     lsof
     moreutils
     ncdu
+    nerd-fonts.jetbrains-mono
     nix-output-monitor
     nodejs_22
     p7zip
@@ -226,22 +227,14 @@
 
   programs.kitty = {
     enable = true;
-    font = {
-      name = "JetBrainsMono Nerd Font Mono";
-      package = pkgs.nerd-fonts.jetbrains-mono;
-      size = 18;
-    };
-    themeFile = "Monokai_Soda";
     extraConfig = ''
-      startup_session ~/launch.kitty-session
-      map shift+up previous_window
-      map shift+down next_window
-      map shift+left previous_tab
-      map shift+right next_tab
-      map shift+f2 set_window_title
-      symbol_map U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b7,U+e700-U+e8ef,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f381,U+f400-U+f533,U+f0001-U+f1af0 JetBrainsMono Nerd Font Mono
+      include extra.conf
     '';
   };
+  home.file.".config/kitty/extra.conf".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/dotconfig/kitty/kitty.conf";
+  home.file.".config/kitty/theme.conf".source =
+    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.config/home-manager/dotconfig/kitty/theme.conf";
 
   home.stateVersion = "25.05";
 
