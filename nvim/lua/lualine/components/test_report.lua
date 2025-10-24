a4322e3aa7bfed21e13ea36c5bec7922587cb594
Akshay Kumar <132216831+bmblb3@users.noreply.github.com>
2025-10-24 16:31:15 +0200

    rename vars for alignment

--- nvim/lua/lualine/components/test_report.lua
+++ nvim/lua/lualine/components/test_report.lua
@@ -1,10 +1,10 @@
 local M = require("lualine.component"):extend()
 
 M.default_config = {
-  icon_total = "󰙨",
+  icon_pool = "󰙨",
   icon_pass = "✓",
   icon_fail = "✗",
-  color_total = { fg = "#666666" },
+  color_pool = { fg = "#666666" },
   color_pass = { fg = "#00bc11" },
   color_fail = { fg = "#ff5f5f" },
 }
@@ -17,22 +17,21 @@ end
 function M:update_status()
   local test_state = vim.b.test_state
   if not test_state or test_state.total == 0 then return "" end
+  local pool = test_state.total
+  local fail = test_state.failing
+  local pass = pool - fail
 
   local parts = {}
   local h = require("lualine.highlight")
 
-  local hl_total = h.create_component_highlight_group(self.options.color_total, "test_total", self.options)
+  local hl_pool = h.create_component_highlight_group(self.options.color_pool, "test_pool", self.options)
   local hl_pass = h.create_component_highlight_group(self.options.color_pass, "test_pass", self.options)
   local hl_fail = h.create_component_highlight_group(self.options.color_fail, "test_fail", self.options)
 
-  table.insert(parts, h.component_format_highlight(hl_total) .. self.options.icon_total .. "" .. test_state.total .. ":")
-
-  local passing = test_state.total - test_state.failing
-  table.insert(parts, h.component_format_highlight(hl_pass) .. self.options.icon_pass .. "" .. passing)
-
-  table.insert(parts, h.component_format_highlight(hl_total) .. "|")
-
-  table.insert(parts, h.component_format_highlight(hl_fail) .. self.options.icon_fail .. "" .. test_state.failing)
+  table.insert(parts, h.component_format_highlight(hl_pool) .. self.options.icon_pool .. "" .. pool .. ":")
+  table.insert(parts, h.component_format_highlight(hl_pass) .. self.options.icon_pass .. "" .. pass)
+  table.insert(parts, h.component_format_highlight(hl_pool) .. "|")
+  table.insert(parts, h.component_format_highlight(hl_fail) .. self.options.icon_fail .. "" .. fail)
 
   return table.concat(parts, "")
 end
